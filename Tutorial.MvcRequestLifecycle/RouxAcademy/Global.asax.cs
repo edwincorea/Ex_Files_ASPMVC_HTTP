ce3008a67016abb507ac2980cadcb8eef2cb3b17
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-07-08 16:20:01 -0600

    Explore global application events.

--- Tutorial.MvcRequestLifecycle/RouxAcademy/Global.asax.cs
+++ Tutorial.MvcRequestLifecycle/RouxAcademy/Global.asax.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +13,14 @@ namespace RouxAcademy
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Application state
+            Application["ApplicationStartDateTime"] = DateTime.Now;
+        }
+
+        protected void Application_End()
+        {
+            Application.Clear();
         }
     }
 }
