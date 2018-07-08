8f937eeb0d28fbe19c49b37bf78252b86e982763
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-07-08 17:19:58 -0600

    Implement a custom async HTTP handler.

---
+++ Tutorial.MvcRequestLifecycle/CustomHttpHandler/AsynchronousHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using System.Web;
+
+namespace CustomHttpHandler
+{
+    public class AsynchronousHandler : HttpTaskAsyncHandler
+    {
+        public override Task ProcessRequestAsync(HttpContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
