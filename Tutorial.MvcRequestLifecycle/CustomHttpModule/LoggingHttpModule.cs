49dc1bad004451bd3d37a5fc2348c84ce12b3f89
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-07-08 17:02:24 -0600

    Build a custom request processing module.

---
+++ Tutorial.MvcRequestLifecycle/CustomHttpModule/LoggingHttpModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+
+namespace CustomHttpModule
+{
+    public class LoggingHttpModule : IHttpModule
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.LogRequest += Application_LogRequest;
+        }
+
+        private void Application_LogRequest(object sender, EventArgs e)
+        {
+            HttpApplication application = sender as HttpApplication;
+            HttpContext context = application.Context;
+
+            string requestPath = context.Request.Path;
+
+            Trace.WriteLine(String.Format("Request Path: {0}", requestPath));
+        }
+    }
+}
